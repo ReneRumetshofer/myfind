a7a7bd70f670a80d2b977bd51281d7b84300be23
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2024-09-13 15:09:33 +0200

    adds comments

--- main.cpp
+++ main.cpp
@@ -52,35 +52,42 @@ int main(int argc, char** argv) {
     string searchPath = filePaths[0];
     filePaths.erase(filePaths.begin());
 
-    // If searchPath is a valid folder
+    // Check if searchPath is a valid folder, otherwise return with error
     if(!fs::is_directory(searchPath)) {
         cerr << "Error: " << searchPath << " is not a valid path to a directory" << endl;
         return EXIT_FAILURE;
     }
 
     for(auto it = filePaths.begin(); it != filePaths.end(); it++) {
+        // Distinguish between case sensitive and case insensitive search
         string fileNameToSearch = *it;
         if(caseInsensitiveMode) {
             transform(fileNameToSearch.begin(), fileNameToSearch.end(), fileNameToSearch.begin(), ::tolower);
         }
 
         try {
+            // Iterate through all files in the directory and subdirectories
             auto directoryIterator = fs::recursive_directory_iterator(searchPath);
             for (const auto& entry : directoryIterator) {
+                // Change behaviour of iterator to mimmick non-recursive iterator
+                // This is done to avoid if/else with almost identical content
                 if(!recursiveMode) {
                     directoryIterator.disable_recursion_pending();
                 }
 
+                // Get the current file name (iteration) and depending on case sensitivity, transform it to lower case
                 string currentFileName = entry.path().filename();
                 if(caseInsensitiveMode) {
                     transform(currentFileName.begin(), currentFileName.end(), currentFileName.begin(), ::tolower);
                 }
 
+                // File match -> print resulting path
                 if (entry.is_regular_file() && currentFileName == fileNameToSearch) {
                     std::cout << "File found: " << entry.path() << std::endl;
                 }
             }
         } catch (const fs::filesystem_error& err) {
+            // Unknown error will be printed
             cerr << "Error: " << err.what() << endl;
         }
     }
