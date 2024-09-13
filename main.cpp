ec4821c83ff4160f16ec9414885142188d449252
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2024-09-13 15:04:43 +0200

    adds directory search

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <algorithm>
+
 using namespace std;
+namespace fs = std::filesystem;
 
 void printUsage(string programName);
 
@@ -45,16 +49,49 @@ int main(int argc, char** argv) {
             filePaths.push_back(currentArgument);
         }
     }
+    string searchPath = filePaths[0];
+    filePaths.erase(filePaths.begin());
 
-    // Print file paths
-    for(string filePath : filePaths) {
-        cout << filePath << endl;
+    // If searchPath is a valid folder
+    if(!fs::is_directory(searchPath)) {
+        cerr << "Error: " << searchPath << " is not a valid path to a directory" << endl;
+        return EXIT_FAILURE;
+    }
+
+    for(auto it = filePaths.begin(); it != filePaths.end(); it++) {
+        string fileNameToSearch = *it;
+        if(caseInsensitiveMode) {
+            transform(fileNameToSearch.begin(), fileNameToSearch.end(), fileNameToSearch.begin(), ::tolower);
+        }
+
+        try {
+            auto directoryIterator = fs::recursive_directory_iterator(searchPath);
+            for (const auto& entry : directoryIterator) {
+                if(!recursiveMode) {
+                    directoryIterator.disable_recursion_pending();
+                }
+
+                string currentFileName = entry.path().filename();
+                if(caseInsensitiveMode) {
+                    transform(currentFileName.begin(), currentFileName.end(), currentFileName.begin(), ::tolower);
+                }
+
+                if (entry.is_regular_file() && currentFileName == fileNameToSearch) {
+                    std::cout << "File found: " << entry.path() << std::endl;
+                }
+            }
+        } catch (const fs::filesystem_error& err) {
+            cerr << "Error: " << err.what() << endl;
+        }
     }
 }
 
 void printUsage(string programName) {
-    cerr << "Usage: " << programName << " [OPTION] file1 file2 ..." << endl;
+    cerr << "Usage: " << programName << " [OPTIONS] searchpath file1 file2 ..." << endl;
     cerr << "Options:" << endl;
     cerr << "  -i: interactive mode" << endl;
-    cerr << "  -R: recursive mode" << endl;
+    cerr << "  -R: recursive mode (includes sub-folders)" << endl;
+    cerr << "Arguments:" << endl;
+    cerr << "  searchpath: path to search for files" << endl;
+    cerr << "  file1, file2, ...: file names to search for" << endl;
 }
