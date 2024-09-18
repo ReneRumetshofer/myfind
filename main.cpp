a62ab686de368e8cac3d7bed4f6f234900535e1b
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2024-09-18 15:48:27 +0200

    adds waiting for child processes

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <filesystem>
 #include <algorithm>
+#include <sys/wait.h>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 void printUsage(string programName);
+void searchFile(bool caseInsensitiveMode, bool recursiveMode, string searchPath, string file);
 
 int main(int argc, char** argv) {
     bool caseInsensitiveMode = false;
@@ -30,7 +33,7 @@ int main(int argc, char** argv) {
             break;
         default:
             printUsage(argv[0]);
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -38,7 +41,7 @@ int main(int argc, char** argv) {
     if(optind == argc) {
         cerr << "No file names provided" << endl;
         printUsage(argv[0]);
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     // Parse file paths from argv excluding options
@@ -55,42 +58,44 @@ int main(int argc, char** argv) {
     // Check if searchPath is a valid folder, otherwise return with error
     if(!fs::is_directory(searchPath)) {
         cerr << "Error: " << searchPath << " is not a valid path to a directory" << endl;
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    for(auto it = filePaths.begin(); it != filePaths.end(); it++) {
-        // Distinguish between case sensitive and case insensitive search
-        string fileNameToSearch = *it;
-        if(caseInsensitiveMode) {
-            transform(fileNameToSearch.begin(), fileNameToSearch.end(), fileNameToSearch.begin(), ::tolower);
+    // Fork for each file to search for
+    for (auto it = filePaths.begin(); it != filePaths.end(); it++) {
+        pid_t pid = fork();
+
+        // Failure while forking
+        if (pid == -1) {
+            cerr << "Error while forking! Aborting." << endl;
+            exit(EXIT_FAILURE);
         }
+        // Child process that performs the search
+        else if (pid == 0) {
+            searchFile(caseInsensitiveMode, recursiveMode, searchPath, *it);
+            exit(EXIT_SUCCESS);
+        }
+    }
 
-        try {
-            // Iterate through all files in the directory and subdirectories
-            auto directoryIterator = fs::recursive_directory_iterator(searchPath);
-            for (const auto& entry : directoryIterator) {
-                // Change behaviour of iterator to mimmick non-recursive iterator
-                // This is done to avoid if/else with almost identical content
-                if(!recursiveMode) {
-                    directoryIterator.disable_recursion_pending();
-                }
-
-                // Get the current file name (iteration) and depending on case sensitivity, transform it to lower case
-                string currentFileName = entry.path().filename();
-                if(caseInsensitiveMode) {
-                    transform(currentFileName.begin(), currentFileName.end(), currentFileName.begin(), ::tolower);
-                }
-
-                // File match -> print resulting path
-                if (entry.is_regular_file() && currentFileName == fileNameToSearch) {
-                    std::cout << "File found: " << entry.path() << std::endl;
-                }
+    // Wait for all child processes to finish
+    while(true) {
+        int status;
+        pid_t exitedPid = wait(&status);
+        if (exitedPid == -1) {
+            if (errno == ECHILD) {
+                break;
             }
-        } catch (const fs::filesystem_error& err) {
-            // Unknown error will be printed
-            cerr << "Error: " << err.what() << endl;
+            cerr << "Error while waiting for child processes" << endl;
+            exit(EXIT_FAILURE);
+        } else {
+            if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+                cerr << "Sub-process with PID " << exitedPid << " failed" << endl;
+                exit(EXIT_FAILURE);
+            }
         }
     }
+
+    exit(EXIT_SUCCESS);
 }
 
 void printUsage(string programName) {
@@ -101,4 +106,38 @@ void printUsage(string programName) {
     cerr << "Arguments:" << endl;
     cerr << "  searchpath: path to search for files" << endl;
     cerr << "  file1, file2, ...: file names to search for" << endl;
+}
+
+void searchFile(bool caseInsensitiveMode, bool recursiveMode, string searchPath, string file) {
+    // Distinguish between case sensitive and case insensitive search
+    string fileNameToSearch = file;
+    if(caseInsensitiveMode) {
+        transform(fileNameToSearch.begin(), fileNameToSearch.end(), fileNameToSearch.begin(), ::tolower);
+    }
+
+    try {
+        // Iterate through all files in the directory and subdirectories
+        auto directoryIterator = fs::recursive_directory_iterator(searchPath);
+        for (const auto& entry : directoryIterator) {
+            // Change behaviour of iterator to mimmick non-recursive iterator
+            // This is done to avoid if/else with almost identical content
+            if(!recursiveMode) {
+                directoryIterator.disable_recursion_pending();
+            }
+
+            // Get the current file name (iteration) and depending on case sensitivity, transform it to lower case
+            string currentFileName = entry.path().filename();
+            if(caseInsensitiveMode) {
+                transform(currentFileName.begin(), currentFileName.end(), currentFileName.begin(), ::tolower);
+            }
+
+            // File match -> print resulting path
+            if (entry.is_regular_file() && currentFileName == fileNameToSearch) {
+                std::cout << "File found: " << entry.path() << std::endl;
+            }
+        }
+    } catch (const fs::filesystem_error& err) {
+        // File system error will be printed
+        cerr << "Error: " << err.what() << endl;
+    }
 }
